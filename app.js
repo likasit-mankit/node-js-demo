6618ff1d794c973867d07a2dd29eb4290fab84b7
likasit(windows) <tung25455@gmail.com>
2026-02-18 20:27:21 +0700

    feat: Implement basic routing for '/' and '/about' with a 404 handler and update content type header.

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const http = require("http");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
+  res.statusCode = 200;
 
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  if (req.url === "/") {
     res.end("Hello World");
+  } else if (req.url === "/about") {
+    res.end("About Page");
+  } else {
+    res.statusCode = 404;
+    res.end("ไม่พบหน้าเว็บ");
+  }
 });
 
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
